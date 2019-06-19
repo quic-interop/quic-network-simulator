e9091a3fc0a94ec7882a0890a247f38ad8e07075
Marten Seemann <martenseemann@gmail.com>
2019-06-19 10:50:28 +0800

    delete unused route on the server endpoint

--- endpoint/run.sh
+++ endpoint/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 route add -net 10.0.0.0 netmask 255.0.0.0 gw $GATEWAY
+# delete unused routes
 route del -net 10.0.0.0 netmask 255.255.0.0
+route del -net 10.100.0.0 netmask 255.255.0.0
 
 sleep 3600
