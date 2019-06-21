4190b3091f2add0cead90707f1e7c91d87a9e31f
Marten Seemann <martenseemann@gmail.com>
2019-06-21 16:08:46 +0800

    enable TCP / UDP checksum calculation on the endpoints

--- endpoint/run.sh
+++ endpoint/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# By default, docker containers don't compute UDP / TCP checksums.
+# When packets run through ns3 however, the receiving endpoint requires valid checksums.
+# This command makes sure that the endpoints set the checksum on outgoing packets.
+ethtool -K eth0 tx off
+
 route add -net 10.0.0.0 netmask 255.0.0.0 gw $GATEWAY
 # delete unused routes
 route del -net 10.0.0.0 netmask 255.255.0.0
