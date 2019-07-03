052ba06f9812a8a8b075a999b34cecf5a53a11da
Jana Iyengar <jri.ietf@gmail.com>
2019-07-02 23:20:30 -0700

    remove the CSMA network

--- sim/sim.cc
+++ sim/sim.cc
@@ -8,7 +8,6 @@
 #include "ns3/internet-apps-module.h"
 #include "ns3/ipv4-static-routing-helper.h"
 #include "ns3/ipv4-list-routing-helper.h"
-#include "ns3/csma-module.h"
 #include "ns3/point-to-point-module.h"
 
 
@@ -35,45 +34,22 @@ int main(int argc, char *argv[]) {
   GlobalValue::Bind("SimulatorImplementationType", StringValue("ns3::RealtimeSimulatorImpl"));
   GlobalValue::Bind("ChecksumEnabled", BooleanValue(true));
 
-  // The topology has a CSMA network of 4 nodes on the left side.
   NodeContainer nodesLeft;
-  nodesLeft.Create(4);
-
-  CsmaHelper csmaLeft;
-  csmaLeft.SetChannelAttribute("DataRate", DataRateValue (5000000));
-  csmaLeft.SetChannelAttribute("Delay", TimeValue (MilliSeconds (1)));
-
-  NetDeviceContainer devicesLeft = csmaLeft.Install(nodesLeft);
+  nodesLeft.Create(1);
   InternetStackHelper internetLeft;
   internetLeft.Install(nodesLeft);
 
-  Ipv4AddressHelper ipv4Left;
-  ipv4Left.SetBase("10.1.0.0", "255.255.0.0", "0.0.0.10");
-  Ipv4InterfaceContainer interfacesLeft = ipv4Left.Assign(devicesLeft);
-
-  // Create the right side CSMA network of 4 nodes.
   NodeContainer nodesRight;
-  nodesRight.Create(4);
-
-  CsmaHelper csmaRight;
-  csmaRight.SetChannelAttribute("DataRate", DataRateValue(5000000));
-  csmaRight.SetChannelAttribute("Delay", TimeValue(MilliSeconds(1)));
-
-  NetDeviceContainer devicesRight = csmaRight.Install(nodesRight);
-
+  nodesRight.Create(1);
   InternetStackHelper internetRight;
   internetRight.Install(nodesRight);
-  
-  Ipv4AddressHelper ipv4Right;
-  ipv4Right.SetBase("10.99.0.0", "255.255.0.0", "0.0.0.10");
-  Ipv4InterfaceContainer interfacesRight = ipv4Right.Assign(devicesRight);
 
   // Stick in the point-to-point line between the sides.
   PointToPointHelper p2p;
-  p2p.SetDeviceAttribute("DataRate", StringValue("512Mbps"));
+  p2p.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
   p2p.SetChannelAttribute("Delay", StringValue("10ms"));
 
-  NodeContainer nodes = NodeContainer(nodesLeft.Get(3), nodesRight.Get(3));
+  NodeContainer nodes = NodeContainer(nodesLeft.Get(0), nodesRight.Get(0));
   NetDeviceContainer devices = p2p.Install(nodes);
 
   Ipv4AddressHelper ipv4;
@@ -84,10 +60,6 @@ int main(int argc, char *argv[]) {
   installNetDevice(nodesLeft.Get(0), "eth0", Mac48AddressValue("02:51:55:49:43:00"), Ipv4InterfaceAddress("10.0.0.2", "255.255.0.0"));
   NS_LOG_INFO("Create eth1");
   installNetDevice(nodesRight.Get(0), "eth1", Mac48AddressValue("02:51:55:49:43:01"), Ipv4InterfaceAddress("10.100.0.2", "255.255.0.0"));
-
-  // enable pcaps for all nodes
-  csmaLeft.EnablePcapAll("tap-wifi-dumbbell", false);
-  csmaRight.EnablePcapAll("tap-wifi-dumbbell", false);
 
   Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
