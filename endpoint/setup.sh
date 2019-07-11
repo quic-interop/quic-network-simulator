c6f6b5eb5a9d04723f7181c9d85ddc40420a4d2c
Marten Seemann <martenseemann@gmail.com>
2019-07-11 14:32:04 +0700

    remove the sleep command from the endpoint setup script

--- endpoint/setup.sh
+++ endpoint/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Setting up routes..."
 # By default, docker containers don't compute UDP / TCP checksums.
 # When packets run through ns3 however, the receiving endpoint requires valid checksums.
 # This command makes sure that the endpoints set the checksum on outgoing packets.
@@ -10,4 +11,3 @@ route add -net 10.0.0.0 netmask 255.0.0.0 gw $GATEWAY
 route del -net 10.0.0.0 netmask 255.255.0.0
 route del -net 10.100.0.0 netmask 255.255.0.0
 
-sleep 3600
