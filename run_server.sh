88ce1fb4bb343fae4c0475fcb39934ba079a46f1
Jana Iyengar <jri.ietf@gmail.com>
2019-08-29 16:35:41 -0700

    add mount point for logs

--- run_server.sh
+++ run_server.sh
@@ -6,4 +6,4 @@ IMPL=$1
 shift
 
 docker rm server &> /dev/null || true
-docker run --cap-add=NET_ADMIN --network rightnet --hostname server --name server --ip 192.168.100.100 -it $IMPL server "$@"
+docker run --cap-add=NET_ADMIN -v `pwd`/qnslogs:/qnslogs --network rightnet --hostname server --name server --ip 192.168.100.100 -it $IMPL server "$@"
