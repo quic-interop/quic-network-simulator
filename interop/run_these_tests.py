87be2832285d2413217bfedd0edd728aa5024a3c
Marten Seemann <martenseemann@gmail.com>
2019-09-24 17:48:52 +0700

    add a command line option to specify the test case

--- interop/run_these_tests.py
+++
@@ -1,9 +0,0 @@
-import testcases
-
-# These tests will be run
-TESTCASES = [ 
-  testcases.TestCaseHandshake(),
-  testcases.TestCaseTransfer(),
-  testcases.TestCaseRetry(),
-  testcases.TestCaseResumption(),
-]
