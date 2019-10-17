70ab571df6685e8ac66862ed84d12fa9aef96459
Marten Seemann <martenseemann@gmail.com>
2019-10-17 08:47:59 -0700

    move interop runner to github.com/marten-seemann/quic-interop-runner

--- interop/implementations.py
+++
@@ -1,7 +0,0 @@
-# add your QUIC implementation here
-IMPLEMENTATIONS = { # name => docker image
-  "quicgo": "martenseemann/quic-go-interop:latest",
-  "quicly": "janaiyengar/quicly:interop",
-  "ngtcp2": "ngtcp2/ngtcp2-interop:latest",
-  "quant":  "ntap/quant:interop"
-}
