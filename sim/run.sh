2b4c79efa2b00a0b7e27109cf38faba384e5e4f7
Marten Seemann <martenseemann@gmail.com>
2020-10-14 13:32:48 +0700

    don't change the MAC addresses assigned to the network interfaces

--- sim/run.sh
+++ sim/run.sh
@@ -3,14 +3,8 @@
 set -e
 
 # We are using eth0 and eth1 as EmuFdNetDevices in ns3.
-# ns3 usually uses MAC address spoofing to separate ns3 from other traffic,
-# see https://www.nsnam.org/docs/models/html/fd-net-device.html#emufdnetdevicehelper.
-# We actually want all traffic to flow through ns3, so we manually set the MAC
-# addresses, and use the configure the same values in the ns3 script,
-# thereby effectively disabling the spoofing.
-ifconfig eth0 hw ether 02:51:55:49:43:00
+# Use promiscuous mode to allow ns3 to capture all packets.
 ifconfig eth0 promisc
-ifconfig eth1 hw ether 02:51:55:49:43:01
 ifconfig eth1 promisc
 
 # A packet arriving at eth0 destined to 10.100.0.0/16 could be routed directly to eth1,
