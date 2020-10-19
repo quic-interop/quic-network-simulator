d5c5e6aef8c619fecfd7a58faf4dcd1deca2bb85
Marten Seemann <martenseemann@gmail.com>
2020-10-19 17:14:41 +0700

    implement a function to check if a packet is a UDP packet

--- sim/scenarios/helper/quic-packet.cc
+++ sim/scenarios/helper/quic-packet.cc
@@ -6,10 +6,40 @@
 #include "ns3/packet.h"
 #include "ns3/ppp-header.h"
 #include "ns3/ipv4-header.h"
+#include "ns3/ipv6-header.h"
 #include "ns3/udp-header.h"
 
 using namespace ns3;
 using namespace std;
+
+
+bool IsUDPPacket(Ptr<Packet> p) {
+    PppHeader ppp_hdr = PppHeader();
+    p->RemoveHeader(ppp_hdr);
+    bool is_udp = false;
+    switch(ppp_hdr.GetProtocol()) {
+        case 0x21: // IPv4
+            {
+                Ipv4Header hdr = Ipv4Header();
+                p->PeekHeader(hdr);
+                is_udp = hdr.GetProtocol() == 17;
+            }
+            break;
+        case 0x57: // IPv6
+            {
+                Ipv6Header hdr = Ipv6Header();
+                p->PeekHeader(hdr);
+                is_udp = hdr.GetNextHeader() == 17;
+            }
+            break;
+        default:
+            cout << "Unknown PPP protocol: " << ppp_hdr.GetProtocol() << endl;
+            break;
+    }
+    p->AddHeader(ppp_hdr);
+    return is_udp;
+}
+
 
 QuicPacket::QuicPacket(Ptr<Packet> p) : p_(p) {
     const uint32_t p_len = p->GetSize();
@@ -17,6 +47,7 @@ QuicPacket::QuicPacket(Ptr<Packet> p) : p_(p) {
     p->CopyData(buffer, p_len);
     ppp_hdr_ = PppHeader();
     uint32_t ppp_hdr_len = p->RemoveHeader(ppp_hdr_);
+    // TODO: This currently only works for IPv4.
     ipv4_hdr_ = Ipv4Header();
     uint32_t ip_hdr_len = p->RemoveHeader(ipv4_hdr_);
     udp_hdr_ = UdpHeader();
