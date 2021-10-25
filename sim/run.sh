043f0ac84c416288608239571114534a0b2d60ae
sedrubal <dev@sedrubal.de>
2021-10-25 17:04:15 +0200

    Add support for nftables

--- sim/run.sh
+++ sim/run.sh
@@ -11,21 +11,32 @@ ifconfig eth1 promisc
 # and a packet arriving at eth1 destined to 10.0.0.0/16 directly to eth0.
 # This would allow packets to skip the ns3 simulator altogether.
 # Drop those to make sure they actually take the path through ns3.
-iptables -A FORWARD -i eth0 -o eth1 -j DROP
-iptables -A FORWARD -i eth1 -o eth0 -j DROP
-ip6tables -A FORWARD -i eth0 -o eth1 -j DROP
-ip6tables -A FORWARD -i eth1 -o eth0 -j DROP
-
-if [[ -n "$WAITFORSERVER" ]]; then
-  wait-for-it-quic -t 10s $WAITFORSERVER
+if iptables -L 2> /dev/null | grep FORWARD > /dev/null; then
+    echo "Using iptables"
+    iptables -A FORWARD -i eth0 -o eth1 -j DROP
+    iptables -A FORWARD -i eth1 -o eth0 -j DROP
+    ip6tables -A FORWARD -i eth0 -o eth1 -j DROP
+    ip6tables -A FORWARD -i eth1 -o eth0 -j DROP
+elif grep nf_tables /proc/modules > /dev/null ; then
+    echo "Using nftables"
+    nft add table ip filter
+    nft add chain ip filter FORWARD
+    nft add rule ip filter FORWARD iifname "eth0" oifname "eth1" counter drop
+    nft add rule ip filter FORWARD iifname "eth1" oifname "eth0" counter drop
+else
+    echo "Neither ip_tables nor nf_tables module is loaded. Skipping firewall configuration."
 fi
 
-echo "Using scenario:" $SCENARIO
+if [[ -n "$WAITFORSERVER" ]]; then
+    wait-for-it-quic -t 10s "${WAITFORSERVER}"
+fi
+
+echo "Using scenario: ${SCENARIO}"
 
 eval ./scratch/"$SCENARIO &"
 
-PID=`jobs -p`
-trap "kill -SIGINT $PID" INT
-trap "kill -SIGTERM $PID" TERM
-trap "kill -SIGKILL $PID" KILL
+PID=$(jobs -p)
+trap "kill -SIGINT ${PID}" INT
+trap "kill -SIGTERM ${PID}" TERM
+trap "kill -SIGKILL ${PID}" KILL
 wait
