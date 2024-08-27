de21ef07aeac544e3440f9738a90f52e6de503ce
Lars Eggert <lars@eggert.org>
2024-08-27 05:32:45 +0300

    Add burst parameter to DropRateErrorModel (#134)
    * Add burst parameter to DropRateErrorModel
    
    This makes it so that the error model doesn't create loss bursts that
    are longer than the burst parameter. This should prevent idle timeouts
    when loss bursts are too long. It will however lower the overall loss
    rate below what is configured, because we're not adjusting the drop rate
    upward to compensate for breaking loss bursts (by forcing a forward).
    
    * Minimize diff
    
    * Simplify
    
    * Suggestions from @marten-seemann

--- sim/scenarios/drop-rate/drop-rate-error-model.cc
+++ sim/scenarios/drop-rate/drop-rate-error-model.cc
@@ -1,3 +1,5 @@
+#include <iomanip>
+
 #include "../helper/quic-packet.h"
 #include "drop-rate-error-model.h"
 
@@ -12,30 +14,62 @@ TypeId DropRateErrorModel::GetTypeId(void) {
         ;
     return tid;
 }
- 
+
 DropRateErrorModel::DropRateErrorModel()
-    : rate(0), distr(0, 99) {
+    : rate(0), distr(0, 99), burst(INT_MAX), dropped_in_a_row(0), dropped(0), forwarded(0)
+{
     std::random_device rd;
     rng = new std::mt19937(rd());
 }
 
-void DropRateErrorModel::DoReset(void) { }
+void DropRateErrorModel::DoReset(void) {
+    dropped_in_a_row = 0;
+    dropped = 0;
+    forwarded = 0;
+}
  
 bool DropRateErrorModel::DoCorrupt(Ptr<Packet> p) {
     if(!IsUDPPacket(p)) return false;
 
-    QuicPacket qp = QuicPacket(p);
-
-    if (distr(*rng) >= rate) {
-        cout << "Forwarding packet (" << qp.GetUdpPayload().size() << " bytes) from " << qp.GetIpv4Header().GetSource() << endl;
-        qp.ReassemblePacket();
-        return false;
+    bool shouldDrop = false;
+    if (dropped_in_a_row >= burst) {
+        dropped_in_a_row = 0;
+        shouldDrop = false;
+    } else if (distr(*rng) < rate) {
+        dropped_in_a_row++;
+        shouldDrop = true;
+    } else {
+        dropped_in_a_row = 0;
+        shouldDrop = false;
     }
 
-    cout << "Dropping packet (" << qp.GetUdpPayload().size() << " bytes) from " << qp.GetIpv4Header().GetSource() << endl;
-    return true;
+    QuicPacket qp = QuicPacket(p);
+
+    if (shouldDrop) {
+        cout << "Dropping ";
+        dropped++;
+    } else {
+        cout << "Forwarding ";
+        forwarded++;
+        qp.ReassemblePacket();
+    }
+    cout << qp.GetUdpPayload().size()
+         << " bytes " << qp.GetIpv4Header().GetSource() << ":"
+         << qp.GetUdpHeader().GetSourcePort() << " -> "
+         << qp.GetIpv4Header().GetDestination() << ":"
+         << qp.GetUdpHeader().GetDestinationPort()
+         << ", dropped " << dropped << "/" << dropped + forwarded << " ("
+         << fixed << setprecision(1)
+         << (double)dropped / (dropped + forwarded) * 100
+         << "%)" << endl;
+
+    return shouldDrop;
 }
 
 void DropRateErrorModel::SetDropRate(int rate_in) {
     rate = rate_in;
+}
+
+void DropRateErrorModel::SetMaxDropBurst(int burst_in) {
+    burst = burst_in;
 }
--- sim/scenarios/drop-rate/drop-rate-error-model.h
+++ sim/scenarios/drop-rate/drop-rate-error-model.h
@@ -8,16 +8,23 @@
 using namespace ns3;
 
 // The DropRateErrorModel drops random packets, at a user-specified drop rate. 
+// But no more than a user-specified number of packets in a row.
 class DropRateErrorModel : public ErrorModel {
  public:
     static TypeId GetTypeId(void);
     DropRateErrorModel();
     void SetDropRate(int perc);
+    void SetMaxDropBurst(int burst);
     
  private:
     int rate;
     std::mt19937 *rng;
     std::uniform_int_distribution<> distr;
+    int next_rate;
+    int burst;
+    int dropped_in_a_row;
+    int dropped;
+    int forwarded;
 
     bool DoCorrupt (Ptr<Packet> p);
     void DoReset(void);
