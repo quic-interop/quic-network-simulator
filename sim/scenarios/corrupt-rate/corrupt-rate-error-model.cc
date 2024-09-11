f36915bf65a14aa3ed712caeec4ca157d882b6d7
Lars Eggert <lars@eggert.org>
2024-09-10 20:43:14 -0700

    sim: add burst parameter to `CorruptRateErrorModel` (#137)
    * WIP
    
    * Add burst parameter to CorruptRateErrorModel
    
    Equivalent of #134
    
    * Fix log output
    
    * Update sim/scenarios/corrupt-rate/README.md
    
    Co-authored-by: Max Inden <mail@max-inden.de>
    
    * Suggestion from @mxinden
    
    * Fix off-by-one
    
    * Undo. Issue was something else.
    
    ---------
    
    Co-authored-by: Max Inden <mail@max-inden.de>

--- sim/scenarios/corrupt-rate/corrupt-rate-error-model.cc
+++ sim/scenarios/corrupt-rate/corrupt-rate-error-model.cc
@@ -1,13 +1,10 @@
+#include <iomanip>
+
 #include <cstdint>
 #include <vector>
 
 #include "corrupt-rate-error-model.h"
 #include "../helper/quic-packet.h"
-#include "ns3/header.h"
-#include "ns3/packet.h"
-#include "ns3/ppp-header.h"
-#include "ns3/ipv4-header.h"
-#include "ns3/udp-header.h"
 
 using namespace std;
 
@@ -20,48 +17,87 @@ TypeId CorruptRateErrorModel::GetTypeId(void) {
         ;
     return tid;
 }
- 
+
 CorruptRateErrorModel::CorruptRateErrorModel()
-    : rate(0), distr(0, 99) {
+    : rate(0), distr(0, 99), burst(INT_MAX), corrupted_in_a_row(0), corrupted(0), forwarded(0) {
     std::random_device rd;
     rng = new std::mt19937(rd());
 }
 
-void CorruptRateErrorModel::DoReset(void) { }
+void CorruptRateErrorModel::DoReset(void) {
+    corrupted_in_a_row = 0;
+    corrupted = 0;
+    forwarded = 0;
+}
 
 bool CorruptRateErrorModel::DoCorrupt(Ptr<Packet> p) {
     if(!IsUDPPacket(p)) return false;
 
     QuicPacket qp = QuicPacket(p);
 
-    if(distr(*rng) >= rate) {
-        cout << "Forwarding packet (" << qp.GetUdpPayload().size() << " bytes) from " << qp.GetIpv4Header().GetSource() << endl;
-        qp.ReassemblePacket();
-        return false;
+    bool shouldCorrupt = false;
+    if (qp.IsVersionNegotiationPacket()) {
+        // Don't corrupt Version Negotiation packets.
+        // Version Negotiation packets are expected to be sent when the version
+        // field of the Initial was corrupted. Client are supposed to ignore
+        // Version Negotiation packets that contain the version that they
+        // offered.
+        corrupted_in_a_row = 0;
+        shouldCorrupt = false;
+    } else if (corrupted_in_a_row >= burst) {
+        corrupted_in_a_row = 0;
+        shouldCorrupt = false;
+    } else if (distr(*rng) < rate) {
+        corrupted_in_a_row++;
+        shouldCorrupt = true;
+    } else {
+        corrupted_in_a_row = 0;
+        shouldCorrupt = false;
     }
 
-    // Don't corrupt Version Negotiation packets.
-    // Version Negotiation packets are expected to be sent when the version field of the Initial was corrupted.
-    // Client are supposed to ignore Version Negotiation packets that contain the version that they offered.
-    if(qp.IsVersionNegotiationPacket()) {
-        qp.ReassemblePacket();
-        return false;
-    }
-    // Corrupt a byte in the 50 bytes of the UDP payload.
-    // This way, we will frequenetly hit the QUIC header.
-    std::uniform_int_distribution<> d(0, min(uint32_t(50), p->GetSize() - 1));
-    int pos = d(*rng);
-    vector<uint8_t>& payload = qp.GetUdpPayload();
-    // Replace the byte at position pos with a random value.
-    while(true) {
-        uint8_t n = std::uniform_int_distribution<>(0, 255)(*rng);
-        if(payload[pos] == n) continue;
-        cout << "Corrupted packet (" << qp.GetUdpPayload().size() << " bytes) from " << qp.GetIpv4Header().GetSource() << " at offset " << pos << " (0x" << std::hex << (unsigned int) payload[pos] << " -> 0x" << (unsigned int) n << ")" << std::dec << endl;
-        payload[pos] = n;
-        break;
+    int pos = 0;
+    uint8_t old_n = 0;
+    uint8_t new_n = 0;
+    if (shouldCorrupt) {
+        cout << "Corrupting ";
+        corrupted++;
+
+        // Corrupt a byte in the 50 bytes of the UDP payload.
+        // This way, we will frequently hit the QUIC header.
+        std::uniform_int_distribution<> d(0, min(uint32_t(50), p->GetSize() - 1));
+        pos = d(*rng);
+        vector<uint8_t> & payload = qp.GetUdpPayload();
+        // Replace the byte at position pos with a random value.
+        while (true) {
+            uint8_t n = std::uniform_int_distribution<>(0, 255)(*rng);
+            if (payload[pos] == n)
+                continue;
+            old_n = payload[pos];
+            payload[pos] = n;
+            new_n = payload[pos];
+            break;
+        }
+    } else {
+        cout << "Forwarding ";
+        forwarded++;
     }
     qp.ReassemblePacket();
-    return false;
+
+    cout << qp.GetUdpPayload().size() << " bytes "
+         << qp.GetIpv4Header().GetSource() << ":"
+         << qp.GetUdpHeader().GetSourcePort() << " -> "
+         << qp.GetIpv4Header().GetDestination() << ":"
+         << qp.GetUdpHeader().GetDestinationPort();
+    if (pos != 0)
+        cout <<  " offset " << pos << " 0x" << std::hex
+             << (unsigned int)old_n << " -> 0x" << (unsigned int)new_n
+             << std::dec;
+    cout << ", corrupted "
+         << corrupted << "/" << corrupted + forwarded << " (" << fixed
+         << setprecision(1) << (double)corrupted / (corrupted + forwarded) * 100
+         << "%)" << endl;
+
+    return shouldCorrupt;
 }
 
 void CorruptRateErrorModel::SetCorruptRate(int rate_in) {
--- sim/scenarios/corrupt-rate/corrupt-rate-error-model.h
+++ sim/scenarios/corrupt-rate/corrupt-rate-error-model.h
@@ -8,16 +8,22 @@
 using namespace ns3;
 
 // The CorruptRateErrorModel corrupts random packets, at a user-specified rate.
+// But no more than a user-specified number of packets in a row.
 class CorruptRateErrorModel : public RateErrorModel {
  public:
     static TypeId GetTypeId(void);
     CorruptRateErrorModel();
     void SetCorruptRate(int perc);
+    void SetMaxCorruptBurst(int burst);
     
  private:
     int rate;
     std::mt19937 *rng;
     std::uniform_int_distribution<> distr;
+    int burst;
+    int corrupted_in_a_row;
+    int corrupted;
+    int forwarded;
 
     bool DoCorrupt (Ptr<Packet> p);
     void DoReset(void);
